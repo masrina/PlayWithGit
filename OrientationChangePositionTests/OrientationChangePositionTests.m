43dfb1e7e29c50d4ef733dda85e89d54e799ddd8
Masrina <rina.masrina@iproperty.com>
2015-07-10 14:02:35 +0800

    14th

--- OrientationChangePositionTests/OrientationChangePositionTests.m
+++ OrientationChangePositionTests/OrientationChangePositionTests.m
@@ -34,7 +34,7 @@
     // This is an example of a performance test case.
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
-        NSLog(@"Develop Branch 13th build");
+        NSLog(@"Develop Branch 14th build");
     }];
 }
 
