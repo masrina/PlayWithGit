0a519c2619f67cb803c97c03b7761a6ebd31012f
Masrina <rina.masrina@iproperty.com>
2015-07-10 13:18:17 +0800

    16th

--- OrientationChangePosition/ViewController.m
+++ OrientationChangePosition/ViewController.m
@@ -166,7 +166,7 @@
 //    return _verticalOrientationConstraints;
 //}
 
-// 15th changes will trigger build to automation_test branch
+// 16th changes will trigger build to automation_test branch
 
 - (NSArray *)horizontalOrientationConstraints
 {
